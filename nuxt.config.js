a41efc6e2768885bd72cd644fec9688ca0730188
Khoironi Kurnia Syah <khoironidev@gmail.com>
2021-12-04 12:04:21 +0800

    adding pwa feature

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ["@nuxtjs/pwa"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
@@ -81,11 +81,17 @@ export default {
       name: "IO Economic",
       short_name: "IO Economic",
       description: "IO Economic Model Calculator",
+      display: "standalone",
       lang: "en",
     },
     icon: {
       filename: "icon.png",
       sizes: [64, 120, 144, 152, 192, 384, 512],
+    },
+    workbox: {
+      /* workbox options */
+      dev: true,
+      offlineStrategy: "StaleWhileRevalidate",
     },
   },
 };
